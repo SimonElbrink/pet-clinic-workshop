e029428c74a5d5d07282913307d798478d3feda1
Simon Elbrink <Simon.Elbrink@gmail.com>
2020-06-23 12:20:25 +0200

    Revert "Created Entity + Repo"
    This reverts commit 6c74e71e

--- src/main/java/se/lexicon/petclinic/repository/PetRepository.java
+++
@@ -1,7 +0,0 @@
-package se.lexicon.petclinic.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import se.lexicon.petclinic.entity.Pet;
-
-public interface PetRepository extends CrudRepository<Pet, String> {
-}
--- src/main/java/se/lexicon/petclinic/repository/PetTypeRepository.java
+++
@@ -1,7 +0,0 @@
-package se.lexicon.petclinic.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import se.lexicon.petclinic.entity.PetType;
-
-public interface PetTypeRepository extends CrudRepository<PetType, Integer> {
-}
